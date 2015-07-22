a5a648a8799122c34f8b1be0c35073595008eead
Chris Corbett <pixelscript@users.noreply.github.com>
2015-07-22 16:07:55 +0100

    I've made them move

--- main.js
+++ main.js
@@ -1,72 +1,187 @@
 $(function(){
-	var arrays = [];
-	var maxPasses = 4;
-	var passes = 0;
+	var ranges = [];
+	var maxPasses = 3;
 	var suppressionRate = 0.15;
-	function genratePoints(min,max,array,ammount){
-		var newPoints = [];
+	var increaseBy = 5;
+	var positionArray = [];
+	var sectionHeight = h / (colourArray.length-1);
+	var startScale = 2;
+	var endScale = 1.2
+
+	function reset() {
+		ctx.fillStyle = 'rgb('+colourArray[0][0]+','+colourArray[0][1]+','+colourArray[0][2]+')';;
+		ctx.fillRect (0, 0, w, h);
+	}
+
+	$('body').on('touchstart mouseup',function(){
+		reset();
+	})
+
+	// ================ GENERATE RANGES ================ //
+
+	function generateRanges(){
+		var scale = 2;
+		for(var i=0; i<colourArray.length-1;i++){
+			var yStart = (sectionHeight*i);
+
+			var p = [0,0];
+			for(var m=0;m<maxPasses;m++) {
+				p = generateRange(p,m);
+			}
+			p = randomifyDetail(p);
+			ranges.push({
+				"points" : p,
+				"colour" : colourArray[i],
+				"yStart" : yStart,
+				"scale" : scale,
+			})	
+			scale *= 0.8;
+
+			positionArray.push(yStart);
+		}
+		positionArray.push(h);
+	}
+
+	function generateRange(array,pass){
+		var max =  100,
+			newPoints = [];
+
 		for (var i=0;i<array.length-1;i++){
 			var difference = 0;
 			if (array[i+1]){
-				var difference = array[i+1] - array[i];
+				difference = array[i+1] - array[i];
 			}
 			newPoints.push(array[i]);
-			for(var m = 0; m<ammount; m++){
-				var basePoint = array[i]+((difference/ammount)*m);
+			for(var m = 0; m<increaseBy; m++){
+				var basePoint = array[i]+((difference/increaseBy)*m);
 				if(isNaN(basePoint)){
 					basePoint = 0;
 				}
-				newPoints.push(basePoint+getRandomInt(min,max));
+				var num = max;
+				for (var p=0;p<pass;p++){
+					num *= suppressionRate;
+				}
+				newPoints.push(basePoint+getRandomInt(0-num,num));
 			}
 		}
 		newPoints.push(array[array.length-1]);
-		passes++;
-		if(passes<maxPasses){
-			array = genratePoints(min*suppressionRate,max*suppressionRate,newPoints,5);
-			arrays.push(array.slice(0));
-		}
-		return array;
+		return newPoints.slice(0);
 	}
-	function drawPath(array,colour){
-		var gap = w/array.length;
-		ctx.beginPath();
-		ctx.moveTo(0,array[0]);
-		for(var i=0; i<array.length;i++){
+
+	function randomifyDetail(array){
+		var newArray = []
+		for(var i=0;i<array.length-1;i++){
 			var skip = getRandomInt(0,1);
-			if(skip && i!= array.length-1){
+			if(skip){
 				continue;
 			}
+			newArray.push(array[i]);
+		}
+		return newArray;
+	}
+	var heightAnimate = 0;
+	function animateRange(){
+		for (var i=0;i<ranges.length;i++){
+			var range = ranges[i];
+			range.yStart++;
+			var closest = closestDivision(range.yStart);
+			range.colour = colourBlending(closest.index,closest.percent);
+		}
+		heightAnimate++
+		if(heightAnimate>sectionHeight){
+			heightAnimate = 0;
+			incrementStack();
+		}
+	}
+	function incrementStack(){
+		ranges.splice(ranges.length-1,1);
+		ranges.unshift(generateNewRange());
+	}
+
+	function generateNewRange(){
+		var p = [0,0];
+		for(var m=0;m<maxPasses;m++) {
+			p = generateRange(p,m);
+		}
+		p = randomifyDetail(p);
+		return{
+			"points" : p,
+			"colour" : colourArray[0],
+			"yStart" : 0,
+			"scale" : 2,
+		};
+	}
+
+	function colourBlending(index,percent){
+		var colour1 = colourArray[index],
+			colour2 = colourArray[index+1],
+			rDiff = colour2[0]-colour1[0],
+			gDiff = colour2[1]-colour1[1],
+			bDiff = colour2[2]-colour1[2],
+			newR = Math.round((rDiff*percent) + colour1[0]),
+			newG = Math.round((gDiff*percent) + colour1[1]),
+			newB = Math.round((bDiff*percent) + colour1[2]);
+		return [newR,newG,newB];
+	}
+
+	function closestDivision(y){
+		var diff = h;
+		var chosenIndex = 0;
+		var secondIndex = 0;
+		var actualIndex = 0
+		for (var i=0; i<positionArray.length;i++){
+			var val = Math.abs(positionArray[i]-y);
+			if (val<diff){
+				diff = Math.abs(positionArray[i]-y);
+				secondIndex = chosenIndex;
+				chosenIndex = i;
+			} else if (val<Math.abs(positionArray[secondIndex]-y)){
+				secondIndex = i;
+			}
+		}
+		actualIndex = Math.min(secondIndex,chosenIndex);
+
+		var range = positionArray[actualIndex+1]-positionArray[actualIndex];
+		var startPoint = y-positionArray[actualIndex];
+		var percent = Math.max(Math.min(startPoint/range,1),0);
+		return {index:actualIndex,percent:percent};
+		
+	}
+
+
+	// ==================== DRAWING ==================== //
+
+	function drawCycle(){
+		reset();
+		for (var i=0;i<ranges.length-1;i++){
+			var range = ranges[i];
+			drawRange(range);
+		}
+		requestAnimationFrame(drawCycle);
+	}
+
+	function drawRange(range){
+		drawPath(range.points,range.colour,range.yStart,range.scale);
+	}
+
+	function drawPath(array,colour,yStart,scale){
+		var gap = w/array.length;
+		ctx.beginPath();
+		ctx.moveTo(0,array[0]+yStart);
+		for(var i=0; i<array.length;i++){
 			var x = (gap*i) + gap,
-				y = array[i];
+				y = (array[i]*scale) + yStart;
 			ctx.lineTo(x,y);
 		}
 		ctx.lineTo(w,h);
 		ctx.lineTo(0,h);
 		ctx.closePath();
-		ctx.fillStyle = colour;
+		ctx.fillStyle = 'rgb('+colour[0]+','+colour[1]+','+colour[2]+')';;
 		ctx.fill();
 	}
 
-	function drawRange(){
-		var increments = h / (colourArray.length);
-		var range = increments*1.2;
-		var firstRange = range;
-		var spread = increments;
-		for(var i=0; i<colourArray.length;i++){
-			var num = (spread*i)+firstRange;
-			passes = 0;
-			pointArray = genratePoints(0-range,range,[num,num],5);
-			drawPath(pointArray,colourArray[i+1]);	
-			range*=0.9;
-		}
-	}
-	function reset() {
-		ctx.fillStyle = colourArray[0];
-		ctx.fillRect (0, 0, w, h);
-		drawRange();
-	}
-	drawRange();
-	$('body').on('touchstart mouseup',function(){
-		reset();
-	})
+	// ================== GET STARTED ================
+	generateRanges();
+	requestAnimationFrame(drawCycle);
+	setInterval(animateRange,1);
 });
--- setup.js
+++ setup.js
@@ -1,6 +1,17 @@
 var canvas, ctx, w, h;
 // var colourArray = ['#ded4de','#bbbbc6','#a0a6b2','#8d979f','#78858c','#707b86','#414754']
-var colourArray = ['#faf8fb','#f0f0f4','#e2e6ec','#d4dbe1','#becbd2','#a0b2c2','#647089']
+// var colourArray = ['#faf8fb','#f0f0f4','#e2e6ec','#d4dbe1','#becbd2','#a0b2c2','#647089']
+var colourArray = [ [250,248,251],
+					[240,240,244],
+					[226,230,236],
+					[212,219,225],
+					[190,203,210],
+					[160,178,194],
+					[100,112,137],
+					[50,58,65]]
+	// var colourArray = [[255,0,0],
+	// 				[0,255,0],
+	// 				[0,0,255]]
 
 $(function(){
 	var $element = $('body'),
@@ -29,7 +40,7 @@ $(function(){
 		$element.append(canvas);
 		canvas.attr('width',w+'px');
 		canvas.attr('height',h+'px');
-		ctx.fillStyle = colourArray[0];
+		ctx.fillStyle = 'rgb('+colourArray[0][0]+','+colourArray[0][1]+','+colourArray[0][2]+')';
 		ctx.fillRect (0, 0, w, h);
 	}
 });
